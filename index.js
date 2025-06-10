583aa93db24c89167bb7c41f4b455f6f7927897d
zainabometere621 <zainabometere621@gmail.com>
2025-06-11 00:57:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var switch_on_button = document.getElementById("switch-on");
+var switch_off_button = document.getElementById("switch-off");
+var image = document.getElementById("bulb")
+
+
+function switchOn(){
+    image.setAttribute("src","light-on.jpg");
+}
+
+function switchOff(){
+    image.setAttribute("src","light-off.jpg");
+}
+
+
